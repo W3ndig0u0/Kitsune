1e9df21da2d2e4513194b989450e190a005c66da
W3ndig0 <Jingxiang304@gmail.com>
2026-03-28 13:42:42 +0100

    kitsune api is online

--- server/src/main/java/com/example/kitsuneApi/controller/AnimeController.java
+++ server/src/main/java/com/example/kitsuneApi/controller/AnimeController.java
@@ -1,12 +1,14 @@
 package com.example.kitsuneApi.controller;
 
 import lombok.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("${properties.api.anime}")
+@CrossOrigin("*")
 public class AnimeController {
 
     @GetMapping("/status")
