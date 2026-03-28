dce8bdf739dbfdb6dbc8a1b85e2320eb69794378
W3ndig0 <Jingxiang304@gmail.com>
2026-03-28 13:51:25 +0100

    add controller and user

--- server/src/main/java/com/example/kitsuneApi/controller/AnimeController.java
+++ server/src/main/java/com/example/kitsuneApi/controller/AnimeController.java
@@ -1,18 +1,40 @@
 package com.example.kitsuneApi.controller;
 
+import com.example.kitsuneApi.service.ConsumetService;
 import lombok.Value;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("${properties.api.anime}")
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:5173")
 public class AnimeController {
 
-    @GetMapping("/status")
+    private final ConsumetService consumetService;
+    public AnimeController(ConsumetService consumetService) {
+        this.consumetService = consumetService;
+    }
+
+    @GetMapping("")
     public String getStatus() {
         return "Kitsune Anime API is online";
     }
+
+    @GetMapping("/search")
+    public Mono<ResponseEntity<String>> search(@RequestParam String query) {
+        return consumetService.searchAnime(query)
+                .map(json -> ResponseEntity.ok().body(json))
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+                .onErrorReturn(ResponseEntity.status(500).build());
+    }
+
+    @GetMapping("/info/{id}")
+    public Mono<ResponseEntity<String>> getInfo(@PathVariable String id) {
+        return consumetService.getAnimeInfo(id)
+                .map(json -> ResponseEntity.ok().body(json))
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+                .onErrorReturn(ResponseEntity.status(500).build());
+    }
+
 }
---
+++ server/src/main/java/com/example/kitsuneApi/model/User.java
@@ -0,0 +1,4 @@
+package com.example.kitsuneApi.model;
+
+public class User {
+}
