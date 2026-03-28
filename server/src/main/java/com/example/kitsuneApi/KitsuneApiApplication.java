1b3f8a81e92c94ae2d71816b04e236d09a6e3502
W3ndig0 <Jingxiang304@gmail.com>
2026-03-28 13:06:35 +0100

    spring init

---
+++ server/src/main/java/com/example/kitsuneApi/KitsuneApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitsuneApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitsuneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitsuneApiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/kitsuneApi/KitsuneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitsuneApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitsuneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
